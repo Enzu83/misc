3855a8859192f7187f17f5779b298d2b2c80117e
Maxime Chambre <maxime.chambre@datadoghq.com>
2025-12-23 15:59:57 +0100

    c load lib

--- c/load_lib/main.c
+++ c/load_lib/main.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 
 int main(void) {
-    void *lib = dlopen("/opt/datadog-agent/etc/checks.d/libdatadog-agent-example.dylib", RTLD_LAZY | RTLD_GLOBAL);
+    void *lib = dlopen("/opt/datadog-agent/etc/checks.d/libdatadog-agent-example.dylib", RTLD_NOW | RTLD_LOCAL);
     printf("%s\n", dlerror());
 }
